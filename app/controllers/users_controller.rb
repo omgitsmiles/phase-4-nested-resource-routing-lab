4988f8976b17303cedc8d567f3e17fdde2f4cda4
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-05 16:31:49 -0400

    commit tree 2498d25252e471e80597ac46622970b27b0f3e61

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+      user = User.find_by(id: params[:id])
+      render json: user, include: :items
+  end
+  
+  private
+
+  def render_not_found
+    render json: { error: "User Not Found" }, status: 404
   end
 
 end
